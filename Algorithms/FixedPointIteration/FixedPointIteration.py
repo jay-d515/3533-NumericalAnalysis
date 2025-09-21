3d0924a63b402ec2246782be14badb8b685bc89c
Jadyn Dangerfield <156955919+jay-d515@users.noreply.github.com>
2025-09-21 21:07:40 +0000

    created fixed point method

--- Algorithms/FixedPointIteration/FixedPointIteration.py
+++ Algorithms/FixedPointIteration/FixedPointIteration.py
@@ -1,1 +1,20 @@
+import math
 
+def fixed_point_iteration(f, p0, tol, max_iterations):
+    i = 1
+
+    while i <= max_iterations:
+        # compute next approximation
+        p = f(p0)
+
+        # check stopping criteria
+        if abs(p - p0) < tol:
+            return p
+        
+        # increment number of iterations
+        i += 1
+        # update for next iteration
+        p0 = p
+
+    print(f"The method failed after {max_iterations} iterations.")
+    return None
