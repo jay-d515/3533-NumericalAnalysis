a193e51b32c1c636c79e1f0a400a240cbfd20c64
Jadyn Dangerfield <156955919+jay-d515@users.noreply.github.com>
2025-09-22 19:48:03 +0000

    edited newton's method

--- Algorithms/NewtonsMethod/NewtonsMethod.py
+++ Algorithms/NewtonsMethod/NewtonsMethod.py
@@ -2,25 +2,27 @@ import math
 from sympy import symbols, diff
 
 def newtons_method(func, func_prime, p0, tol, max_iterations):
-    iteration = 0
+    i = 1
 
-    while iteration <= max_iterations:
+    while i < max_iterations:
+        
         p = p0 - func(p0) / func_prime(p0)
 
         if abs(p - p0) < tol:
-            return p, iteration
+            return p, i
 
-
-        iteration += 1
+        i += 1
         p0 = p
 
-    print("The procedure was unsuccessful.")
-    print("The method failed after ", max_iterations)
+    print("The method failed after ", max_iterations, " iterations.")
+    return None, max_iterations
 
-q1 = lambda x: x**3 + 3*x**2 - 1
-prime = lambda x: 3*x**2 + 6*x
+def f(x):
+    return x**2 - 2*x*math.exp(-x) + math.exp(-2*x)
+def fprime(x):
+    return 2*x + 2*x*math.exp(-x) - 2*math.exp(-x) - 2*math.exp(-2*x)
 
-root, iterations = newtons_method(q1, prime, -3, 10e-4, 50)
+root, iterations = newtons_method(f, fprime, 0.5, 1e-5, 50)
 
 print("Approximation: ", root)
 print("Total iterations: ", iterations)
